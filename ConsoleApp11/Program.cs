add9b1fd72814cdda61d7a1370878a70c358c45a
GusLed1870 <gustavo.lede@gmail.com>
2025-03-21 13:02:06 -0300

    Version Final

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -11,21 +11,26 @@ namespace ConsoleApp11
             a1.avanzar();
             a1.detenerse();
 
+            Computadora c1 = new Computadora("HP", "INTEL", 16);
+            c1.encenderse();
+            c1.apagarse();
+
         }
 
         class Auto {
             public string color;
             public int anio;
             public int cant_ruedas;
-            /*Metodos*/
+            /*constructor*/
             public Auto(string color, int anio, int cant_ruedas)
             {
                 this.color = color;
                 this.anio = anio;
                 this.anio = cant_ruedas;
 
-
+               
             }
+            /*Metodos*/
             public void avanzar()
             {
                 Console.WriteLine("El auto avanzo");
@@ -36,6 +41,32 @@ namespace ConsoleApp11
                 Console.WriteLine("El auto se detuvo");
             }
 
+        }
+        class Computadora
+        {
+            public string marca;
+            public string procesador;
+            public int memoria_ram;
+
+            /*Metodos*/
+            public void encenderse()
+            {
+                Console.WriteLine("La computadora encendio");
+            }
+            public void apagarse()
+            {
+                Console.WriteLine("La computadora se apago");
+            }
+
+            /*constructor*/
+            public Computadora(string marca, string procesador, int memoria_ram)
+            {
+                this.marca = marca;
+                this.procesador = procesador;
+                this.memoria_ram = memoria_ram;
+
+
+            }
         }
     }
 }
