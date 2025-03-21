27556fe8188efe0c5338268a03597acb47ad9c74
GusLed1870 <gustavo.lede@gmail.com>
2025-03-21 09:47:11 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        class auto {
+            public string color;
+            public int anio;
+            public int cant_ruedas;
+        
+        }
+    }
+}
