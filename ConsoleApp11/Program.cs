c8ab9aa687b33a28f3f3cfa057fd01b266922a79
GusLed1870 <gustavo.lede@gmail.com>
2025-03-21 12:42:26 -0300

    prueba

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -6,14 +6,36 @@ namespace ConsoleApp11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Construyendo un auto");
+            Auto a1 = new Auto("Rojo",5212,4);
+            a1.avanzar();
+            a1.detenerse();
+
         }
 
-        class auto {
+        class Auto {
             public string color;
             public int anio;
             public int cant_ruedas;
-        
+            /*Metodos*/
+            public Auto(string color, int anio, int cant_ruedas)
+            {
+                this.color = color;
+                this.anio = anio;
+                this.anio = cant_ruedas;
+
+
+            }
+            public void avanzar()
+            {
+                Console.WriteLine("El auto avanzo");
+            }
+
+            public void detenerse()
+            {
+                Console.WriteLine("El auto se detuvo");
+            }
+
         }
     }
 }
